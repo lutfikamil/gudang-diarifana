567ae3312ea833874119b6074538de359565080c
lutfikamil <61917514+lutfikamil@users.noreply.github.com>
2026-02-06 08:56:07 +0700

    Update tenant_service.dart
    edit karena salah penamaan 'users' yang seharusnya 'members'

--- lib/services/tenant_service.dart
+++ lib/services/tenant_service.dart
@@ -1,5 +1,4 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-//import 'auth_service.dart';
 
 class TenantService {
   static Future<String> createTenant({
@@ -11,24 +10,37 @@ class TenantService {
 
     final tenantRef = firestore.collection('tenants').doc();
 
-    await tenantRef.set({
+    final batch = firestore.batch();
+
+    // Tenant
+    batch.set(tenantRef, {
       'name': tenantName,
       'ownerUid': ownerUid,
       'createdAt': FieldValue.serverTimestamp(),
       'status': 'active',
     });
 
-    await tenantRef.collection('users').doc(ownerUid).set({
-      'role': 'owner',
-      'email': ownerEmail,
-      'joinedAt': FieldValue.serverTimestamp(),
-    });
+    // Member (WAJIB sesuai rules)
+    batch.set(
+      tenantRef.collection('members').doc(ownerUid),
+      {
+        'role': 'owner',
+        'email': ownerEmail,
+        'joinedAt': FieldValue.serverTimestamp(),
+      },
+    );
 
-    await firestore.collection('users').doc(ownerUid).set({
-      'tenantId': tenantRef.id,
-      'role': 'owner',
-      'email': ownerEmail,
-    });
+    // User global
+    batch.set(
+      firestore.collection('users').doc(ownerUid),
+      {
+        'tenantId': tenantRef.id,
+        'role': 'owner',
+        'email': ownerEmail,
+      },
+    );
+
+    await batch.commit();
 
     return tenantRef.id;
   }
